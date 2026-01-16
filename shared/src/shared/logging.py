0d9caa5be4b7b72260b0f1b70899b9416f07b236
Mafciejewicz <melonik0.9846@gmail.com>
2026-01-16 21:32:53 +0100

    Fix file logging

--- shared/src/shared/logging.py
+++ shared/src/shared/logging.py
@@ -3,9 +3,13 @@ import logging.handlers
 from os import environ, makedirs
 import sys
 
+LOG_FORMAT = "%(asctime)s [%(levelname)s] [%(name)s] (%(funcName)s) %(message)s"
+
+LOG_LEVEL = environ.get("LOG_LEVEL", "INFO").upper()
+
 logging.basicConfig(
-    level=environ.get("LOG_LEVEL", "INFO").upper(),
-    format="%(asctime)s %(levelname)s [%(name)s] (%(funcName)s) %(message)s",
+    level=LOG_LEVEL,
+    format=LOG_FORMAT,
     handlers=[logging.StreamHandler(sys.stdout)],
 )
 
@@ -17,12 +21,13 @@ def get_logger(name: str):
 
     makedirs(dirname, exist_ok=True)
 
-    log.addHandler(
-        logging.handlers.RotatingFileHandler(
-            f"{dirname}/{environ.get('LOG_FILE_NAME', name)}.log",
-            maxBytes=20_000_000,
-            backupCount=3,
-        )
+    file_handler = logging.handlers.RotatingFileHandler(
+        f"{dirname}/{environ.get('LOG_FILE_NAME', name)}.log",
+        maxBytes=20_000_000,
+        backupCount=3,
     )
+    file_handler.setLevel(LOG_LEVEL)
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    log.addHandler(file_handler)
 
     return log
